3c2f33196f31c4fb66a304546300c565ef8e8af0
alina <alinaxweber@gmail.com>
2018-04-30 16:41:18 +0200

    Add some Properties in Unicorn.cs

--- UniPoo/UniPoo/Unicorn.cs
+++ UniPoo/UniPoo/Unicorn.cs
@@ -10,19 +10,73 @@ namespace UniPoo
     {
         private string name;
         private DateTime birthdate;
-        private string RGBColor;
+        private string rgbColor;
         private string description;
-        private List<SpecialAbility> specialAbilityList;
+        private List<SpecialAbility> specialAbilityList; //TODO: instanciate
 
         private string Name
         {
             get
             {
+                return name;
+            }
+            set
+            {
+                if(value.Length <= 20)
+                {
+                    this.name = value;
+                }
+            }
+        }
+        public DateTime Birthdate{ get; set; }
 
+        public string RGBColor
+        {
+            get
+            {
+                return rgbColor;
+            }
+            set
+            {
+                if (value.Length == 5)
+                {
+                    this.rgbColor = value;
+                }
             }
         }
 
-        
+        public string Description
+        {
+            get
+            {
+                return description;
+            }
+            set
+            {
+                if (value.Length <= 500)
+                {
+                    this.rgbColor = value;
+                }
+            }
+        }
+
+        public List<SpecialAbility> SpecialAbilityList
+        {
+            get
+            {
+                return specialAbilityList;
+            }
+            set
+            {
+                //bool noDuplicate = true;
+                //for(int i = this.specialAbilityList)
+                //if (noDuplicate)
+                //{
+                //    this.rgbColor = value;
+                //}
+            }
+        }
+
     }
 
     
