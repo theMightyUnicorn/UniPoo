27baab3e7a7c90b4802456a9bc71334dda468f45
alina <alinaxweber@gmail.com>
2018-05-10 14:55:00 +0200

    Listbox with binding + selecting Unicorn from List works

--- UniPoo/UniPoo/MainWindow.xaml.cs
+++ UniPoo/UniPoo/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -21,50 +22,42 @@ namespace UniPoo
     /// </summary>
     public partial class MainWindow : Window, INotifyPropertyChanged
     {
-        public Unicorn TestUnicorn
+        private Unicorn _selectedUnicorn;
+        public Unicorn SelectedUnicorn
         {
-            get; set;
+            get
+            {
+                return this._selectedUnicorn;
+            }
+            set {
+                Trace.WriteLine("New Unicorn Selected " + value.Name);
+                this._selectedUnicorn = value;
+                OnPropertyChanged("SelectedUnicorn");
+
+            }
         }
 
         public List<Unicorn> TestUnicornList { get; set; }
 
-
-        
-        public string UnicornName
-        {
-            get
-            {
-                return "Name: " + TestUnicorn.Name;
-            }
-        }
-        
-        public string UnicornRGBColor
-        {
-            get
-            {
-                return "Color: " + TestUnicorn.RGBColor;
-            }
-        }
         
         public MainWindow()
         {
+            Trace.WriteLine("Swag");
             InitializeComponent();
             DataContext = this;
-            TestUnicorn = new Unicorn("Annegret", new DateTime(2009, 11, 9), "469872", "Hallo I bims Annegret. Bobs und vagene ich bin fett hihi");
 
             TestUnicornList = new List<Unicorn>();
-            TestUnicornList.Add(new Unicorn("Annegret", new DateTime(2009, 11, 9), "469872", "Hallo I bims Annegret. Bobs und vagene ich bin fett hihi"));
             TestUnicornList.Add(new Unicorn("Aaron", new DateTime(1900, 5, 3), "123456", "Ich bin so dumm"));
-            for (int i = 0; i < TestUnicornList.Count; i++)
-            {
-                NamesBox.Items.Add(TestUnicornList.ElementAt(i).Name);
-            }
+            TestUnicornList.Add(new Unicorn("Annegret", new DateTime(2009, 11, 9), "469872", "Hallo I bims Annegret. Bobs und vagene ich bin fett hihi"));
+
+            SelectedUnicorn = TestUnicornList[1];
         }
 
         public event PropertyChangedEventHandler PropertyChanged;
 
         protected void OnPropertyChanged(String propertyName)
         {
+            Console.WriteLine("SWAG");
             if (PropertyChanged != null)
                 PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
         }
